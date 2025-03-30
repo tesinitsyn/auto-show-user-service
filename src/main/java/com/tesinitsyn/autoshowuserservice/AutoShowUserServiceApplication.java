10cb0a679149f1659086e411dca98ee36ae816ad
tesinitsyn <sitiev174@gmail.com>
2025-03-30 18:14:07 +0300

    Initial commit

---
+++ src/main/java/com/tesinitsyn/autoshowuserservice/AutoShowUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.autoshowuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoShowUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoShowUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesinitsyn/autoshowuserservice/AutoShowUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.autoshowuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoShowUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
